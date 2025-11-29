56d57db4ac2639e4dbcf2388f0772168967bf9a1
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-11-29 20:00:42 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open( file_path )
+    rows = file.readlines()
+    file.close()
+    codon_dict = {}
+    for r in rows :
+       cell = r.strip().split('\t')
+       key = cell[0]
+       value = cell[2]
+       codon_dict[key]=value
+    return codon_dict
+
 
 
